8d5145ebf71f7b4978ab82a2e192b864e7087d55
김현우 <dkanfjgwls@naver.com>
2026-01-01 11:16:27 +0900

    rename: daily, weekly 규칙에 맞게 수정

--- modules/ui/__init__.py
+++ modules/ui/__init__.py
@@ -1,8 +1,8 @@
 """UI 모듈 패키지 - 사이드바 및 탭 UI 컴포넌트"""
 
 from .sidebar import render_sidebar
-from .tabs_records import render_records_tab
-from .tabs_daily_ai import render_ai_evaluation_tab
+from .tabs_weekly import render_records_tab
+from .tabs_daily import render_ai_evaluation_tab
 
 __all__ = [
     'render_sidebar',
