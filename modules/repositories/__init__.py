df3a4075fcfce11fe53c6d02395ec7f4cbd2ba8e
김현우 <dkanfjgwls@naver.com>
2026-02-05 16:12:47 +0900

    feat: UserRepository 추가

--- modules/repositories/__init__.py
+++ modules/repositories/__init__.py
@@ -4,6 +4,7 @@ from .weekly_status import WeeklyStatusRepository
 from .daily_info import DailyInfoRepository
 from .ai_evaluation import AiEvaluationRepository
 from .employee_evaluation import EmployeeEvaluationRepository
+from .user import UserRepository
 
 __all__ = [
     'BaseRepository',
@@ -12,4 +13,5 @@ __all__ = [
     'DailyInfoRepository',
     'AiEvaluationRepository',
     'EmployeeEvaluationRepository',
+    'UserRepository',
 ]
